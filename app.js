061d5ca3898a5657d6010954730fd67c852884e0
Samuel Bétrisey <me@sam.ninja>
2015-04-02 05:57:42 +0200

    Default port >1024 for tests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var request = require('request');
+
 var port = 53;
+if(process.argv[2]) {
+	port = process.argv[2];
+}
 
 http.createServer(function(req, res) {
   console.log(req.url);
--- test.js
+++ test.js
@@ -1,4 +1,9 @@
+var port = 53;
+if(process.argv[2]) {
+	port = process.argv[2];
+}
+
 var http2dns = require('./app');
-var request = require('request').defaults({'proxy':'http://localhost:53'});
+var request = require('request').defaults({'proxy':'http://localhost:' + port});
 
 request('http://google.com/');
